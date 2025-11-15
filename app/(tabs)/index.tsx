b52bc12729aec88c561e2ee564f5a00df7581fa2
alongapa <alonsogarciapastor@gmail.com>
2025-11-14 22:59:58 -0300

    Fix Home tab search screen import path

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SearchTripsScreen from '@/screens/SearchTripsScreen';
+import SearchTripsScreen from '../../screens/SearchTripsScreen';
 
 export default function HomeTab() {
   return <SearchTripsScreen />;
